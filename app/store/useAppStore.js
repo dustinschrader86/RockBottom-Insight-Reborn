4f44d6025b41d1760747845e2c46f3167a7d6211
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-15 14:27:09 -0400

    Implement Zustand store for app state management

--- app/store/useAppStore.js
+++ app/store/useAppStore.js
@@ -1,1 +1,12 @@
+import { create } from 'zustand';
 
+export const useAppStore = create((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+
+  data: [],
+  setData: (data) => set({ data }),
+
+  loading: false,
+  setLoading: (loading) => set({ loading }),
+}));
