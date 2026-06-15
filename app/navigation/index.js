dc4aaefc691bfa188bea04f11db0a772d2f7b915
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-15 14:16:47 -0400

    Update index.js

--- app/navigation/index.js
+++ app/navigation/index.js
@@ -1,1 +1,15 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
 
+const Stack = createNativeStackNavigator();
+
+export default function RootNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="Home" 
+        component={HomeScreen}
+      />
+    </Stack.Navigator>
+  );
+}
