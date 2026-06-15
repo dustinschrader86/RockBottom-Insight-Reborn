03cfc2f542d6b502e14ebddcc8ef4e60a475d6ad
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-15 15:17:11 -0400

    Add image picker utility for selecting screenshots

--- app/utils/picker.js
+++ app/utils/picker.js
@@ -1,1 +1,15 @@
+import * as ImagePicker from 'expo-image-picker';
 
+export const pickScreenshot = async () => {
+  const result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: false,
+    quality: 1,
+  });
+
+  if (result.canceled) {
+    return null;
+  }
+
+  return result.assets[0].uri;
+};
