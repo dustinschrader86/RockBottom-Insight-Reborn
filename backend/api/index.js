edddb5b308c4ac44669f1b5b1b5687b83f62a9f2
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-15 14:54:53 -0400

    Add getAppInfo function to provide application details

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,10 @@
 export const getStatus = () => {
   return { status: "Backend is alive" };
 };
+export const getAppInfo = () => {
+  return {
+    name: "Rockbottom Insight",
+    version: "0.1",
+    author: "Dustin",
+  };
+};
